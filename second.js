62457d172463a2835e4737ba2666ec1288636c81
Marushka <marushkarachelledsilva@gmail.com>
2025-03-02 10:09:13 +0530

    added second.js file

---
+++ second.js
@@ -0,0 +1,9 @@
+const profile={
+    name:"siya",
+    posts:195,
+    followers:569,
+    following:4,
+    aim:"to educate somoone is the highest priority",
+    profession:"ex-mircosoft",
+};
+console.log(profile);
