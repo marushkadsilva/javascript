77c19c2f5a6eed52e648b12152f1e3a1c9c31bb9
Marushka <marushkarachelledsilva@gmail.com>
2025-03-05 19:25:36 +0530

    js

---
+++ loops.js
@@ -0,0 +1,40 @@
+
+//for(let i=1;i <= 5; i++){
+//console.log("hello world");
+//}
+
+//to calculate sum of 1 to 5 numbers
+//let sum=0;
+//for(let i=1;i<=5;i++){
+  //  sum=sum+i;
+//}
+  //  console.log("sum= ",sum);
+
+  //print 1 to 5
+
+  //for (let i=1; i <= 5 ;i++){
+    //console.log("i= ",i);
+  //}
+
+  //while loop
+  //let i=1;
+  //while(i<=5){
+    //console.log("i=",i);
+    //i++;
+ // } 
+
+   //print all even numbers from 0 to 100
+    
+     //for(let num=0 ;num <= 100; num++){
+       // if(num%2===0){
+         //   console.log("even number=",num);
+        //}
+     //}
+
+     //game
+
+     let gamenum=25;
+     let usernum=prompt("guess gamenum:");
+     while(usernum!=gamenum){
+     }
+     console.log("right num");
